adfa691187242f1f0a5cb6f364229d7112de5de3
NIRAJ SHARMA <47564827+binarybytesui@users.noreply.github.com>
2025-11-26 13:46:39 +0530

    updated file @ /src/config/allowedProductFields.php

---
+++ src/config/allowedProductFields.php
@@ -0,0 +1,12 @@
+<?php
+// /src/config/allowedProductFields.php
+return [
+    "category",
+    "name",
+    "part_no",
+    "main_price",
+    "discount_percent",
+    "labour_charges",
+    "wire_cost",
+    "extras"
+];
